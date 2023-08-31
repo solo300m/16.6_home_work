fb3c152a8c9bd5ed3f08b022d50c5fe4d17f3a80
Sergey Lyulchenko <solo300m@yandex.ru>
2023-08-31 15:29:03 +0400

    2_task_commit

--- task_2.cpp
+++ task_2.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 using namespace std;
+
+void inputStrToDouble(string &whole, string &fraction)
+{
+    cout << "Ввести целую и дробную часть числа через пробел: ";
+    cin >> whole >> fraction;
+}
+void parseStrToDouble(string &whole, string &fraction, double &rez)
+{
+    string tmp = whole + "." + fraction;
+    rez = stod(tmp);
+}
+
 int main()
 {
+    string strWhole = "";
+    string strFraction = "";
+    string &whole = strWhole;
+    string &fraction = strFraction;
+    double rez = 0.0;
+    double &ptrRez = rez;
+    inputStrToDouble(whole, fraction);
+    parseStrToDouble(whole, fraction, ptrRez);
+    cout << "Введенное число типа double: " << rez << endl;
 }
